9a74c2886c5356ec67e3c8ac24aedd8a4ea43760
jotave <joao.v.souza193@aluno.senai.br>
2026-06-12 10:48:49 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -14,6 +14,15 @@ app.use(express.json());
 const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
 app.use('/api/', limiter);
 
+// Rota de teste para você verificar se o Back-end está vivo na Vercel
+app.get('/api/teste', (req, res) => {
+    res.json({ 
+        status: "online", 
+        projeto: "Campanha de Leitura SESI",
+        timestamp: new Date()
+    });
+});
+
 // 🔌 CONEXÃO COM AS ROTAS DO SESI
 const alunoRoutes = require('./src/routes/alunoRoutes');
 app.use('/api', alunoRoutes);
@@ -23,6 +32,7 @@ app.use((err, req, res, next) => {
     console.error('💥 Erro:', err.message || err);
     res.status(500).json({ error: 'Erro interno no servidor do projeto de leitura.' });
 });
+
 
 const PORT = process.env.PORT || 3001;
 
